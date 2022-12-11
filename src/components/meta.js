70d8cb3df4d90115fb00986ff3363dd4981a2082
Asko Soukka <asko.soukka@iki.fi>
2022-12-11 21:48:00 +0200

    Upgrade to GatsbyJS 5 (#2)

---
+++ src/components/meta.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+
+function Meta({ title, description, slug, children }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  );
+
+  const metaDescription = description || site.siteMetadata.description;
+
+  return (
+    <>
+      <title>{`${title} | ${site.siteMetadata.title}`}</title>
+      {!!slug ? <link rel="canonical" href={`https://datakurre.pandala.org${slug}`} /> : null}
+      <meta name="description" content={metaDescription} />
+      <meta name="og:type" content="website" />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={metaDescription} />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content={site.siteMetadata.author} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={metaDescription} />
+      {children}
+    </>
+  );
+}
+
+export default Meta;
