009c110539bfdb4211e675d401d4061aa0ee57c5
Asko Soukka <asko.soukka@iki.fi>
2022-12-18 16:19:06 +0200

    Update self-verification links

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,37 @@
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+const React = require('react');
+
+exports.onRenderBody = ({
+  pathname,
+  setHtmlAttributes,
+  setPostBodyComponents,
+}) => {
   setHtmlAttributes({ lang: 'en' });
+  if (pathname === '/') {
+    setPostBodyComponents([
+      // eslint-disable-next-line jsx-a11y/anchor-has-content,jsx-a11y/control-has-associated-label
+      <a
+        href="https://fosstodon.org/@datakurre@pandala.org"
+        rel="me"
+        style={{ display: 'none' }}
+      />,
+      // eslint-disable-next-line jsx-a11y/anchor-has-content,jsx-a11y/control-has-associated-label
+      <a
+        href="https://mas.to/@datakurre@pandala.org"
+        rel="me"
+        style={{ display: 'none' }}
+      />,
+      // eslint-disable-next-line jsx-a11y/anchor-has-content,jsx-a11y/control-has-associated-label
+      <a
+        href="https://mastodon.online/@datakurre@pandala.org"
+        rel="me"
+        style={{ display: 'none' }}
+      />,
+      // eslint-disable-next-line jsx-a11y/anchor-has-content,jsx-a11y/control-has-associated-label
+      <a
+        href="https://social.anoxinon.de/@datakurre@pandala.org"
+        rel="me"
+        style={{ display: 'none' }}
+      />,
+    ]);
+  }
 };
